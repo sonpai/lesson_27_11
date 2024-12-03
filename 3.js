f4a0fa9c55011f5053c56facfc5126ca6f633a10
sonpai <115645068+sonpai@users.noreply.github.com>
2024-12-03 12:43:13 +0200

    3.js

--- 3.js
+++ 3.js
@@ -1,5 +1,5 @@
 'use strict'
-//Sofia Kononenko
+//Sofia Kononenko and Sham Hason
 
 //This program counts the number of zeros in a given array and display the result.
 const numbersArr = [0, 5, 1, 0, 2, 0, 6, 0, 0, 2, 1, 9, 0]; 
